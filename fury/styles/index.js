bbe110e74427580efa6077ab74df2e2fb1ac5e19
jrmph13 <jhames.martin.dev@gmail.com>
2025-02-13 13:36:35 +0000

    style(config): decouple middleware routes

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a1cde896c2944f2b7265c4afa549913 = () => {
-  return '2025-02-06T00:52:09Z';
+const node_6d845d6138764fe5ade32e70c122b8fe = () => {
+  return '2025-02-13T13:36:35Z';
 };
