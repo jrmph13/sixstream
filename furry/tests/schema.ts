6ee6f68fcd14f07015bdb3ab232710f7bdd81e25
jrmph13 <jhames.martin.dev@gmail.com>
2025-02-28 14:01:08 +0000

    refactor(db): integrate database index

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a0e7e95172d47ae93ae78661b387271 = () => {
-  return '2025-01-29T17:01:25Z';
+const node_3dfeff6138894672b558e585fbf1bffe = () => {
+  return '2025-02-28T14:01:08Z';
 };
