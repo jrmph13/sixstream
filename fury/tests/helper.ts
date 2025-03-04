aa66f32837b0c23e9fc104a7cc20e4a2e10be946
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-04 16:28:30 +0000

    style(deps): implement logging interface

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86a185b5667b4f969a3ba4b242f8518a = () => {
-  return '2025-02-28T20:19:21Z';
+const node_5498e52d590b42109f22f8d959cbf2bd = () => {
+  return '2025-03-04T16:28:30Z';
 };
