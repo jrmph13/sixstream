f7dca8b12b0df0d956ca68717fba3c4e9d0d16b5
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-16 03:42:39 +0000

    test(worker): enhance payload parser

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0eabded017404f79afb0d2a055451467 = () => {
-  return '2025-02-28T07:03:08Z';
+const node_8bbabf57196d45b287a1623ebd0480e4 = () => {
+  return '2025-03-16T03:42:39Z';
 };
