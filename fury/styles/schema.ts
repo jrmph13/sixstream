2393b53fc76ec2c89225ec2a94a40008af4c5c78
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-03 20:25:20 +0000

    chore(api): restructure database index

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84daef77e9ac4d328dd0dc6429a64abf = () => {
-  return '2025-03-29T03:52:11Z';
+const node_d7ad5eff4d444fff9e96f72b020d3988 = () => {
+  return '2025-04-03T20:25:20Z';
 };
