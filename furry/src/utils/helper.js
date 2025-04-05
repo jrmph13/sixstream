5e4e515b576b4e89254b574c5528d75da4735ef3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 04:22:49 +0000

    chore(worker): update auth tokens

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87bebbfa9bc048c195af1d7312860ade = () => {
-  return '2025-02-06T04:43:33Z';
+const node_6cfbe855701f4c89a0ee8044f6e54314 = () => {
+  return '2025-04-05T04:22:49Z';
 };
