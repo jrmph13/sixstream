522872023069a5416da96695ed12be898f7da22c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-08 22:46:59 +0000

    perf(utils): restructure auth tokens

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3175eb0ec2114fb686a03b7aab8c3efc = () => {
-  return '2025-04-05T20:25:28Z';
+const node_fe363283a92d4e0bbe7dfb5bfb63a677 = () => {
+  return '2025-04-08T22:46:59Z';
 };
