3f4851907ed8bbb8c99de9522e0216ec053d95e2
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 05:35:56 +0000

    chore(ui): decouple payload parser

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb383996ab2d4b07915d575ca812b5cc = () => {
-  return '2025-04-10T23:02:19Z';
+const node_008c919f7aa94aed8f8fd2fa14c7900b = () => {
+  return '2025-04-10T05:35:56Z';
 };
