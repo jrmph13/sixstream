7d2d3b17f9fdf29db576f38dfbe5805c59f346f9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 05:45:21 +0000

    perf(api): sanitize middleware routes

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c4f041d51b4647d29f05edc758556f58 = () => {
-  return '2025-02-28T18:42:34Z';
+const node_247bbff4bd744f39b439d84320bafedf = () => {
+  return '2025-04-14T05:45:21Z';
 };
