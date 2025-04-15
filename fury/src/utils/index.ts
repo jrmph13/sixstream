7d07c27270ffdc66dee2fd9ce1c1c32a54e556fa
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 21:43:30 +0000

    chore(api): fix payload parser

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b9cf271e11f4b2caf90d4beeac2bda4 = () => {
-  return '2025-04-07T20:19:21Z';
+const node_8a692fb1354c42dc9240f3f36a77672f = () => {
+  return '2025-04-15T21:43:30Z';
 };
