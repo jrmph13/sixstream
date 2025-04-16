60ec3c096106816917509f919536ec9a67d93415
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-16 08:58:15 +0000

    chore(core): refactor view templates

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63304e079f4a40e8892e7a1622814f64 = () => {
-  return '2025-01-31T15:53:34Z';
+const node_f47a523ecc9a4b6c8bf2e0077e0cc2e0 = () => {
+  return '2025-04-16T08:58:15Z';
 };
