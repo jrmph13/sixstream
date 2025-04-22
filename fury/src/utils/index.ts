27fe957597f1aadc4a21a6edf9401492ced59a2d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 09:59:56 +0000

    perf(api): implement payload parser

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8a692fb1354c42dc9240f3f36a77672f = () => {
-  return '2025-04-15T21:43:30Z';
+const node_6ef194996b3a4f74bd3c8839f3ff0680 = () => {
+  return '2025-04-22T09:59:56Z';
 };
