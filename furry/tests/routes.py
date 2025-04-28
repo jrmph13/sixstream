753aa6dd7878d626154509037b4b8c5d81da5025
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 03:36:23 +0000

    fix(auth): restructure database index

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53732c57e00c443e94da0e24794addbe = () => {
-  return '2025-03-07T00:02:12Z';
+const node_b571739c32f5481f824a0e49078c98f7 = () => {
+  return '2025-04-28T03:36:23Z';
 };
