95c135f47cb3f09eb8d9afce253ab02ebab43b21
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 00:44:42 +0000

    feat(core): decouple view templates

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_064083d8e7ed45249a0620e4bfde1c83 = () => {
-  return '2025-03-04T07:56:32Z';
+const node_bc730482bbba4b58966a3f5f61178731 = () => {
+  return '2025-04-28T00:44:42Z';
 };
