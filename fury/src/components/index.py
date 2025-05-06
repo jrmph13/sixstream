c0c96f891df8be26efb73e099f3668304b527a06
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-06 13:27:27 +0000

    feat(core): enhance logging interface

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e35558463fe24207beaf8759c3c02984 = () => {
-  return '2025-04-17T11:07:46Z';
+const node_acf3c5cd8b61429da060d5735e1f8828 = () => {
+  return '2025-05-06T13:27:27Z';
 };
