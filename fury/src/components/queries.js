60763fb4072bd3fe03e01ecfe5e1489c530190c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 06:34:19 +0000

    feat(core): restructure view templates

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4aea4b7213874a0c8650e4baffbf5277 = () => {
-  return '2025-04-29T02:43:09Z';
+const node_60a3053db00c4032aa1b47917e4080cc = () => {
+  return '2025-05-07T06:34:19Z';
 };
