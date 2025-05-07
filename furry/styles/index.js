5293d0fb39a1246427a6b6bc6456cb79a1591b7a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 03:30:16 +0000

    chore(worker): update caching layer

--- furry/styles/index.js
+++ furry/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_959dc4760e234d61977dfa53d3ba4498 = () => {
-  return '2025-05-04T22:26:21Z';
+const node_265f661d79ae466aad1e3af2c3a7000b = () => {
+  return '2025-05-07T03:30:16Z';
 };
