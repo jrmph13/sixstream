c329d5d261a6d57d7783bf7ada9786bb358765c5
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 11:06:25 +0000

    style(worker): sanitize logging interface

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ef194996b3a4f74bd3c8839f3ff0680 = () => {
-  return '2025-04-22T09:59:56Z';
+const node_489352a1445644ca834ec600786137b8 = () => {
+  return '2025-05-08T11:06:25Z';
 };
