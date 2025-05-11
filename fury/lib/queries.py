bcd11d849f7e9d1a2e03421327ac9062c199dc1e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 20:16:23 +0000

    test(utils): implement auth tokens

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e732fc2b6eaf4cd688d2fce2059b2d80 = () => {
-  return '2025-03-11T12:14:42Z';
+const node_cad7d7213798445faa83ffe721ec51cd = () => {
+  return '2025-05-11T20:16:23Z';
 };
