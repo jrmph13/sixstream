b0d22e19d570acf20e558d32042095fd56699836
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-14 22:46:55 +0000

    fix(ui): decouple logging interface

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a56a77ab90984f98a5f54655ad51f762 = () => {
-  return '2025-03-14T08:00:42Z';
+const node_9959eaca06ce495fa4cdc0a165768b3b = () => {
+  return '2025-05-14T22:46:55Z';
 };
