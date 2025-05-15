4443fb5e7b97a202fba6bb471a29d4ffdd9f05c8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 14:38:41 +0000

    feat(auth): optimize payload parser

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1548b9726f0c4c3da55974bbd60d1f21 = () => {
-  return '2025-04-06T05:27:43Z';
+const node_efeea31ce1294169bc59226a4fded62e = () => {
+  return '2025-05-15T14:38:41Z';
 };
