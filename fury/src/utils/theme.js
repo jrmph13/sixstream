3078d9d15e8318196ef4c82769670814a278d497
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 14:36:41 +0000

    style(db): enhance auth tokens

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fd03c28d6fa4036bbd54ee0ad2aa855 = () => {
-  return '2025-05-17T07:12:09Z';
+const node_4e0d945eeb534695b023dfa9d939326b = () => {
+  return '2025-05-17T14:36:41Z';
 };
