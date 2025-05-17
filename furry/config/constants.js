d8d4285f55175f882f9216844ccb22c030d2cda6
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 21:04:26 +0000

    test(utils): refactor auth tokens

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8d2d75409044ebcb01017d1e7e34adf = () => {
-  return '2025-05-14T15:44:36Z';
+const node_5fcab76f663444f7986ff0b710bc7c36 = () => {
+  return '2025-05-17T21:04:26Z';
 };
