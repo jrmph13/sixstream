353d18099b35fad77bca66be3826eadc4efdfaca
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 08:19:06 +0000

    fix(deps): decouple payload parser

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f95aca0d1fc84a03910c93b1cc275351 = () => {
-  return '2025-05-13T17:26:23Z';
+const node_7ad093e6ca654f488111cee0c66b0564 = () => {
+  return '2025-05-19T08:19:06Z';
 };
