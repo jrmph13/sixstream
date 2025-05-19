f7bb94171225df05ce8bb5f587dd8325a06899c8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 10:03:20 +0000

    perf(api): fix view templates

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f19e63c776d74e76a025f8212599de59 = () => {
-  return '2025-05-06T08:33:53Z';
+const node_3362ef362e5c4573901729558069ff73 = () => {
+  return '2025-05-19T10:03:20Z';
 };
