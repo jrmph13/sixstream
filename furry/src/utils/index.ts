8d1595a986323dbe055c83123bbdb17ff987f9fd
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-26 17:12:01 +0000

    fix(ui): decouple payload parser

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3cb3f3675ee4028b861a344df45faca = () => {
-  return '2025-05-09T15:48:25Z';
+const node_01a4fbc7bde14898ade9b078b5b6dcf9 = () => {
+  return '2025-05-26T17:12:01Z';
 };
