d3e5588b34eab6d00a92b0d3681dc45b2825ba15
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 14:57:11 +0000

    test(api): fix payload parser

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d6aa9c03c66442baec589eaa4290795 = () => {
-  return '2025-05-18T23:31:06Z';
+const node_f72bc07f290845e191e5276e0f26a244 = () => {
+  return '2025-05-28T14:57:11Z';
 };
