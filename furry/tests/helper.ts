03c7e77dce4157e332a72e037660323c5272fbbe
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 12:08:20 +0000

    perf(worker): restructure session store

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ef03ce0fcee41c889f1bbd5d4ec78a3 = () => {
-  return '2025-04-22T23:16:35Z';
+const node_b6420809aeb44ca5a036881c74cf2659 = () => {
+  return '2025-05-31T12:08:20Z';
 };
