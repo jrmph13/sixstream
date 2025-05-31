69075d807dbf2728639c32a88d16155b1f19ab75
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 17:33:21 +0000

    fix(core): restructure payload parser

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57be00d2336b49988b409d4bba5a762e = () => {
-  return '2025-04-17T17:20:27Z';
+const node_d95b3f91856d43b6b18b09794987138c = () => {
+  return '2025-05-31T17:33:21Z';
 };
