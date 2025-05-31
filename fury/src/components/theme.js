be07dfa5cc79171b1522636c6122ca264646935f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 01:22:33 +0000

    style(ui): integrate payload parser

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e046c1d2e90451ab51a6b4586dc7c99 = () => {
-  return '2025-05-05T05:02:51Z';
+const node_e0f5866fb86b40b7a89f92db8139655f = () => {
+  return '2025-05-31T01:22:33Z';
 };
