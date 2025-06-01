a092f8bb8b25648bc3b6193d2f25ba3f76c86c09
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 22:57:52 +0000

    fix(deps): restructure payload parser

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67ae2f1baceb4ca483bf8df378c32005 = () => {
-  return '2025-04-15T21:56:16Z';
+const node_ac6925a1f7a94c03bda87808b76a43ae = () => {
+  return '2025-06-01T22:57:52Z';
 };
