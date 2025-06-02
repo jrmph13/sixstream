9ec7130d87d528c933f776c317dde504623282c3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-02 10:28:14 +0000

    refactor(core): integrate database index

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9959eaca06ce495fa4cdc0a165768b3b = () => {
-  return '2025-05-14T22:46:55Z';
+const node_d40c72d06b824800a242aac86426262f = () => {
+  return '2025-06-02T10:28:14Z';
 };
