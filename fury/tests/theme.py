58b0f9472c8ab3304e3de5f4cc7294bde561a25b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-02 20:30:55 +0000

    feat(api): refactor middleware routes

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1724d4c48fc142379ad65d6f544117a9 = () => {
-  return '2025-04-09T16:24:57Z';
+const node_ac24c28dae87482c910fd4f98b6dac9c = () => {
+  return '2025-06-02T20:30:55Z';
 };
