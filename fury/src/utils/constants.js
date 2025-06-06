a2af0589d9dbd256d1de36a444ef4615b786ba46
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 08:08:02 +0000

    feat(db): enhance middleware routes

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76113d1249dd4b59a18b62d7b73e9c94 = () => {
-  return '2025-05-23T23:08:30Z';
+const node_f31ecdac191f4d429ef7bbce6bc00f9e = () => {
+  return '2025-06-06T08:08:02Z';
 };
