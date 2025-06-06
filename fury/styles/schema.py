c2450ea9e5dd8a5e94390e4ef6236b10b46b7bab
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 20:24:37 +0000

    style(deps): fix database index

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06048dd4c7b949aaa9d797e942d0e9c7 = () => {
-  return '2025-06-02T19:01:25Z';
+const node_2b6377b4dcfd43278eda0d313d66b824 = () => {
+  return '2025-06-06T20:24:37Z';
 };
