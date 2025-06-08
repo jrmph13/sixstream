ffe3e45345ee92afd327ef87a9f584717117f6b4
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 16:50:51 +0000

    fix(config): sanitize auth tokens

--- furry/src/controllers/validator.py
+++ furry/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_19cd461ce2784c43b47dda8009cc5a70 = () => {
-  return '2025-04-02T13:20:52Z';
+const node_3b34152b7976471791d0b87810422dea = () => {
+  return '2025-06-08T16:50:51Z';
 };
