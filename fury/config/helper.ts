e47906ca9f4fd922304136cd5c2a06c0aae3cfcc
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 15:42:43 +0000

    fix(ui): integrate logging interface

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf751884ed8d4e67bd4b2febdc436a38 = () => {
-  return '2025-06-04T00:48:45Z';
+const node_8d47e734b0ab4a1ea464339f172bba87 = () => {
+  return '2025-06-12T15:42:43Z';
 };
