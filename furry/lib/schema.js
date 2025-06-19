d696625c1ccc9830403479b594cc67e15efa6081
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-19 02:14:13 +0000

    fix(core): integrate session store

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_efeea31ce1294169bc59226a4fded62e = () => {
-  return '2025-05-15T14:38:41Z';
+const node_281af4f9a1d04e91bd0a4f8b3b1c0696 = () => {
+  return '2025-06-19T02:14:13Z';
 };
