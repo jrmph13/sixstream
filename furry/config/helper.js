7d844c704705ae6cbcd8c59d241170bd93c23400
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 22:47:07 +0000

    perf(core): restructure view templates

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_43e458524a58496aa2cfbc9336e36836 = () => {
-  return '2025-05-25T11:32:41Z';
+const node_da77a1f460af4aa49b018eb63ba08ebe = () => {
+  return '2025-06-21T22:47:07Z';
 };
