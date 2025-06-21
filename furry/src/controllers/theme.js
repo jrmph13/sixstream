fc615f7d009469226eb35163794a54be86f6507f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 14:17:16 +0000

    fix(core): optimize middleware routes

--- furry/src/controllers/theme.js
+++ furry/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d67bc96f20c46388cb024917b898c83 = () => {
-  return '2025-06-07T04:41:35Z';
+const node_d6eae198f5e1410494b8dadbd6c3578f = () => {
+  return '2025-06-21T14:17:16Z';
 };
