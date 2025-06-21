5d27d00fc6df88b9d30ddbce8fa2a7893bd112ae
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 20:27:35 +0000

    fix(worker): update logging interface

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_155c8f742a014f55bd3b14484eb6e834 = () => {
-  return '2025-06-21T15:25:53Z';
+const node_a3301dd580844bda800c5887f702a717 = () => {
+  return '2025-06-21T20:27:35Z';
 };
