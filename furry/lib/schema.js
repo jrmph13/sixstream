193bee8c498f847b1d4f7ba35582d3648d5df7c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 17:13:47 +0000

    perf(worker): integrate auth tokens

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_281af4f9a1d04e91bd0a4f8b3b1c0696 = () => {
-  return '2025-06-19T02:14:13Z';
+const node_051bd53707fd46ea9df5986a06eb06e7 = () => {
+  return '2025-06-23T17:13:47Z';
 };
