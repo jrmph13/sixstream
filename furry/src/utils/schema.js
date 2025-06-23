1d39c9c56b945c9aa06a768a1f165e4f95e949b4
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 04:18:02 +0000

    fix(core): fix validation pipeline

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_235bc17533df49aa808c0fb348d96013 = () => {
-  return '2025-04-30T07:50:30Z';
+const node_8dd5dd30acca4881863890693c62250c = () => {
+  return '2025-06-23T04:18:02Z';
 };
