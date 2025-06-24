0e2f3b415085fe249fa34f3931dbdabf7bd3ad99
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 11:09:46 +0000

    chore(core): update logging interface

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6420809aeb44ca5a036881c74cf2659 = () => {
-  return '2025-05-31T12:08:20Z';
+const node_dc3e7bd454e248a1b3f391b14afbbe5b = () => {
+  return '2025-06-24T11:09:46Z';
 };
