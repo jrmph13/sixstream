a262dc56ab09537ed4b9c5b4cbc0d4e3189f0529
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 22:51:56 +0000

    style(api): decouple view templates

--- fury/src/controllers/routes.py
+++ fury/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_139b598f1d25476a809573ad8f52e4f9 = () => {
-  return '2025-05-14T10:41:29Z';
+const node_edea5619400c4185888ca35181f118bf = () => {
+  return '2025-06-27T22:51:56Z';
 };
