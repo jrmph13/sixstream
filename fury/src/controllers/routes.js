52a0b789e8af888f7728ec848516cc46ed5fbebf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 18:16:55 +0000

    docs(api): refactor payload parser

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f872526825a942e484d08e8530ca9569 = () => {
-  return '2025-06-29T01:12:04Z';
+const node_2902f725c42540de984a9796d6edc6b1 = () => {
+  return '2025-06-30T18:16:55Z';
 };
