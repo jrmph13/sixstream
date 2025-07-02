b7bf61f841063531d24baf168057a5f8e3157d48
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 15:59:06 +0000

    test(utils): sanitize view templates

--- furry/config/constants.ts
+++ furry/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6884e9084604b178a5e19d4e1e6bae1 = () => {
-  return '2025-06-28T16:15:59Z';
+const node_32d5f781e5fa4e5e807a74c4dd6598b6 = () => {
+  return '2025-07-02T15:59:06Z';
 };
