4b7c76188f0b182b6d2854d2d24b33edd4b8181f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 17:05:07 +0000

    test(utils): decouple view templates

--- furry/styles/routes.py
+++ furry/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe0b3bc17bc546a2b35f3c9f5ccc9edc = () => {
-  return '2025-04-22T20:13:03Z';
+const node_90d1b939ff0b45e0ab5102c5eb04a42d = () => {
+  return '2025-07-03T17:05:07Z';
 };
