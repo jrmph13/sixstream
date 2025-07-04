5eed38f9b169aab8f493eafa629c03c7fe17a22c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-04 19:11:28 +0000

    perf(core): optimize session store

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21093728bd0843c3ab5d1a3b3eda04f2 = () => {
-  return '2025-06-29T07:13:13Z';
+const node_ccdc9259e6e74fa296597c310d647446 = () => {
+  return '2025-07-04T19:11:28Z';
 };
