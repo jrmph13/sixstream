7dd3d4280fd3cfc80278c6dfe2d4fd7ebb25efc8
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 07:50:32 +0000

    feat(auth): update view templates

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c12f91fbdb347e8bf0449655f97bde6 = () => {
-  return '2025-06-25T03:59:34Z';
+const node_293b96eb5fe746779dd0d4fd32b5bcfb = () => {
+  return '2025-07-05T07:50:32Z';
 };
