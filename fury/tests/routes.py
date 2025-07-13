270b6d48920e6ca4fe4f652452998b8df389660f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 00:51:27 +0000

    feat(api): restructure payload parser

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d32c62b06d4a44059a6153ac2d02de7b = () => {
-  return '2025-06-17T02:24:07Z';
+const node_a0527501604b4046aa8c764d7bc319c9 = () => {
+  return '2025-07-13T00:51:27Z';
 };
