6729562ca7efc8821167f440d6413bdd5057925a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 05:13:38 +0000

    test(auth): optimize database index

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2c1181e084b148959888cf79cab42c97 = () => {
-  return '2025-06-24T16:53:16Z';
+const node_1d78187b19c448d8959884ff6c6576c7 = () => {
+  return '2025-07-15T05:13:38Z';
 };
