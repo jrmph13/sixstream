cc6fea38dd91723638d3acd4d793eafaf9247d42
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 11:20:12 +0000

    style(worker): enhance payload parser

--- furry/config/validator.py
+++ furry/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a2a727072734aab9b022a68471876ea = () => {
-  return '2025-03-16T17:12:46Z';
+const node_d55aab57fba24bfcb1a034d245434358 = () => {
+  return '2025-07-15T11:20:12Z';
 };
