5de8b77b6f988d88670104b660cd52ee749e7682
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 18:46:34 +0000

    docs(auth): optimize session store

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a6bcb2757984fcaac87a01ac4e3a5a9 = () => {
-  return '2025-05-24T13:53:08Z';
+const node_a243556ba6c54772940fe8ef31b6cee8 = () => {
+  return '2025-07-18T18:46:34Z';
 };
