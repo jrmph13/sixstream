c39df281b6909059987d2cd1455fcd87dea5de9a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 07:27:36 +0000

    chore(config): optimize auth tokens

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5d13454ddd2488cbe778bab77c87e0f = () => {
-  return '2025-06-07T06:32:38Z';
+const node_d99f075db1a24f01aad57ffa1304601f = () => {
+  return '2025-07-19T07:27:36Z';
 };
