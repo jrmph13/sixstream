6b5ecc51074e2dfcc406177338e87ccae062517a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 13:31:54 +0000

    perf(deps): enhance middleware routes

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53e7fe18aed74e6b9ec30edfb2a017e1 = () => {
-  return '2025-04-14T11:35:50Z';
+const node_bc50781105054e08aea8fed2b859d7c5 = () => {
+  return '2025-07-20T13:31:54Z';
 };
