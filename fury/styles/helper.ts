d35314cb23cb29b10808a1b1d044bade5f6d9036
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 02:07:53 +0000

    fix(worker): integrate validation pipeline

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a9ead13f283456ea9802c703afd407f = () => {
-  return '2025-06-02T21:10:35Z';
+const node_63c9d1eeb3b446788ac501f3ed72865d = () => {
+  return '2025-07-24T02:07:53Z';
 };
