003719c0addf0ce396bfb199695e4a3e320827fc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 03:12:20 +0000

    style(auth): integrate view templates

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2dedbda759842d0ab689a5f75dd348a = () => {
-  return '2025-07-19T02:46:02Z';
+const node_aa02855ac3e04bc598ee2cafcce96a74 = () => {
+  return '2025-07-24T03:12:20Z';
 };
