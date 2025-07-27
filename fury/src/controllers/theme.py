5e3a2f4005b0baf126ded9eecfc0cefcc6598f26
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 10:48:26 +0000

    refactor(ui): restructure logging interface

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39b48afdebfc4f9c9cabe491da045343 = () => {
-  return '2025-07-12T00:02:28Z';
+const node_d884dea4aac943c2894751e4db1f4142 = () => {
+  return '2025-07-27T10:48:26Z';
 };
