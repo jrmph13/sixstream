ed85d1e577fcdeee0dfd62c2eb04f7c77ed08a33
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 19:54:14 +0000

    fix(ui): fix payload parser

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a7a0a1383c1423e9de98704775addb4 = () => {
-  return '2025-05-28T06:46:07Z';
+const node_02bba1ecb160415aa24c4bc0d650b2c4 = () => {
+  return '2025-07-28T19:54:14Z';
 };
