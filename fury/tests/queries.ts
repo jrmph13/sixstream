b5f57da07c46169319f4f5ff1249415012e8a730
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 20:42:43 +0000

    fix(auth): integrate validation pipeline

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77f1e08a9e5b4fae8178ad2e52d053a4 = () => {
-  return '2025-07-15T14:38:28Z';
+const node_ef06767dd241494a9ce3594d29719742 = () => {
+  return '2025-08-02T20:42:43Z';
 };
