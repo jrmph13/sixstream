cde487f7d59ca3bb69b9f131d314a3917c58880c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 08:24:52 +0000

    perf(api): restructure auth tokens

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f8710ba53cb4f719aaf2c25edc84f5d = () => {
-  return '2025-08-01T15:12:43Z';
+const node_1bf24139d2e640039f314156459eb866 = () => {
+  return '2025-08-02T08:24:52Z';
 };
