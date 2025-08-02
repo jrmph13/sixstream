8b3837d88327d3e42376762e14a533239dbfaa00
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 09:18:03 +0000

    style(core): optimize auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e009eef55194b7f8d5e8463deb9191b = () => {
-  return '2025-07-19T15:24:37Z';
+const node_a5f224a6a2be40b4a8943ad0656a49a6 = () => {
+  return '2025-08-02T09:18:03Z';
 };
