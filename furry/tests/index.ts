4df78ee11bc655b0abb03bdbdee2c4952ac8acc4
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 12:27:36 +0000

    fix(utils): optimize session store

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a47b743acb14c488c7dec95b16f1d0a = () => {
-  return '2025-07-15T02:56:41Z';
+const node_140495d28a7e42ae960c26736b064227 = () => {
+  return '2025-08-03T12:27:36Z';
 };
