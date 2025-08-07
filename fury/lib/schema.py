7cad2594148f7654577f5ba4a5d5ed46508b18bc
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 04:36:29 +0000

    refactor(config): optimize database index

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d78187b19c448d8959884ff6c6576c7 = () => {
-  return '2025-07-15T05:13:38Z';
+const node_b714fa39ed554ed1aecdaee553a7b8d5 = () => {
+  return '2025-08-07T04:36:29Z';
 };
