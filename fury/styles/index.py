d0b4ad03c253a8ababd3b081fd5f5845ec5c9f13
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 15:43:30 +0000

    refactor(worker): restructure caching layer

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c9046824b8e4f578df7db9c3b12360b = () => {
-  return '2025-07-19T17:56:09Z';
+const node_91dacfc23eae44d9ba78cbe13d6d6004 = () => {
+  return '2025-08-07T15:43:30Z';
 };
