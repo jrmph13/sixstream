448430fb1e1b12ec6d031b54e59a91626753069c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 08:48:53 +0000

    refactor(worker): update auth tokens

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_204956d27a2d4712b0e4c100a1490481 = () => {
-  return '2025-06-29T17:28:29Z';
+const node_2e60f0196fbe421b824046eced9c16f6 = () => {
+  return '2025-08-10T08:48:53Z';
 };
