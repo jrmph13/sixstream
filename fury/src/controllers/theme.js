273f6101b06797de0ebaafa8b6f8d13f98fe6211
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 17:46:47 +0000

    chore(auth): decouple caching layer

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1e06e4ee4d540b6b37e0e7d977c003f = () => {
-  return '2025-07-30T08:56:47Z';
+const node_e03409f3ee1a4581a90666dc11b01d5c = () => {
+  return '2025-08-11T17:46:47Z';
 };
