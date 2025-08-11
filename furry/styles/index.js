0d6417742cbd10ea643b2e6fadde5dacffcf2206
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 12:00:16 +0000

    feat(deps): decouple view templates

--- furry/styles/index.js
+++ furry/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0cb6c42437ec4bd9863333d281a58f6f = () => {
-  return '2025-06-19T18:47:09Z';
+const node_491d3ce1082044b490a4af01af78427f = () => {
+  return '2025-08-11T12:00:16Z';
 };
