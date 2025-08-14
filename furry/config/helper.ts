12b936d7c026032cb7ce6c9d27ee3b3c4433a9ac
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 02:33:09 +0000

    chore(utils): restructure middleware routes

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f8b02cc083d4134916023906305f650 = () => {
-  return '2025-07-10T01:52:26Z';
+const node_4a9edbbe8f0f4a60adb97944efd752e5 = () => {
+  return '2025-08-14T02:33:09Z';
 };
