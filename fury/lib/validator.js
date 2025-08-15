eea4adf3f943e3b4e16adf209987470be4be3b1a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 09:02:59 +0000

    style(worker): update middleware routes

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff639b2e6f3343a999d2dee9da27624b = () => {
-  return '2025-08-13T08:00:26Z';
+const node_514501a0f13e48fe9cc445a68f8c8c94 = () => {
+  return '2025-08-15T09:02:59Z';
 };
