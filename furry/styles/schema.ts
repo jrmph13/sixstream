1c44c09900313b670d26d5a0d1df1f37359b451e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 13:00:05 +0000

    fix(worker): refactor payload parser

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22dc20c235e1448abeb9fb1b3280eb8b = () => {
-  return '2025-07-02T04:28:09Z';
+const node_d97149be7ec64411b36d2c1a1d774781 = () => {
+  return '2025-08-15T13:00:05Z';
 };
