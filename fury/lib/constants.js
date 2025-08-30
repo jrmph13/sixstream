e77720a562a2429c4d7048beb9a309043503d5a0
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 10:11:32 +0000

    fix(worker): integrate auth tokens

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b487b3f73f4c482fbfc664f4cf7846c6 = () => {
-  return '2025-08-12T06:07:24Z';
+const node_ae779306f85a47268a66fab31fa9933d = () => {
+  return '2025-08-30T10:11:32Z';
 };
