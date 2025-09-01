8adb5e0d145a26e13cfb9fc66ab36c4ac61a6baf
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-01 09:38:37 +0000

    chore(worker): enhance middleware routes

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8dc97f7cf17d44269be0162cf80e779a = () => {
-  return '2025-07-28T02:53:08Z';
+const node_3816d45477bc4d01978bd69a016cb237 = () => {
+  return '2025-09-01T09:38:37Z';
 };
