2149f6a37d1bbf78376821dfd224c6535b4e0030
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-06 19:41:09 +0000

    test(api): optimize auth tokens

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_766fd106566c4b54a74b94aa6f3ee2b0 = () => {
-  return '2025-06-14T12:37:24Z';
+const node_ec898cbe85cf4238ab4f0ea7d88cfeb0 = () => {
+  return '2025-09-06T19:41:09Z';
 };
