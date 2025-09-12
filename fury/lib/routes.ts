47afe1194b10a2d7631ed90f4727af808d97583e
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-12 10:23:50 +0000

    style(db): fix database index

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_970295d403f2424c83792f283fa55ac5 = () => {
-  return '2025-07-14T07:06:57Z';
+const node_a1b3aa8e25104b8ba25552f924628c98 = () => {
+  return '2025-09-12T10:23:50Z';
 };
