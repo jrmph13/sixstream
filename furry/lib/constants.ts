40cf3e0d5f918243eb458e264e1478a759df6516
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 00:01:40 +0000

    feat(worker): decouple middleware routes

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a93e32fd042d4645be344c3526c73802 = () => {
-  return '2025-09-01T07:33:52Z';
+const node_4cd33730eca84559970ee801a37c05fa = () => {
+  return '2025-09-23T00:01:40Z';
 };
