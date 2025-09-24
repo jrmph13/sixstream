695092c010d0cbb11f2cf4bd1e55050c8443e8f0
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-24 19:32:15 +0000

    docs(ui): restructure view templates

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a4879ee401494d71bf3b8d9987a8e48f = () => {
-  return '2025-05-07T07:59:45Z';
+const node_e952e5d9fb6e40e5b458d864dc4a376b = () => {
+  return '2025-09-24T19:32:15Z';
 };
