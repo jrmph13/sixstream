4364a88d49ae0f90914a85daa4ec747d359f5e3b
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 02:10:01 +0000

    fix(api): decouple auth tokens

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47b16c15bb334538a9a3d40b33b55ad2 = () => {
-  return '2025-08-30T10:05:38Z';
+const node_1f3591ea484b4ee98d6f62f849ea8c23 = () => {
+  return '2025-09-27T02:10:01Z';
 };
