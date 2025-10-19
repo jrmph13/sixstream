13ee56f6ea9e9b112a0ec1cf9f9e54f67179f246
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-19 06:31:13 +0000

    test(deps): restructure middleware routes

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dd7ba5a2399b4b24a94e3c1f1072fbe9 = () => {
-  return '2025-08-25T08:25:07Z';
+const node_8ed16772f1914ea9b5039f9daf7cf881 = () => {
+  return '2025-10-19T06:31:13Z';
 };
