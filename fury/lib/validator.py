e97b1b90cd3cb590c97acc030dc67c5816e15cd6
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-26 22:07:01 +0000

    fix(auth): optimize view templates

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed8baf174525474e9a8f4cda78c2d3ec = () => {
-  return '2025-08-15T20:24:40Z';
+const node_c790b4a95671447f97763139fb3579ac = () => {
+  return '2025-10-26T22:07:01Z';
 };
