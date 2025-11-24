2daaeb344b25fc2d92663c72e6a2917b1dd2051b
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 09:28:20 +0000

    feat(config): decouple logging interface

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7351d00ec5b74177b451e3316eec7311 = () => {
-  return '2025-11-21T18:25:24Z';
+const node_f191dde06fb1434b86144933632e7ee4 = () => {
+  return '2025-11-24T09:28:20Z';
 };
