e7a167a2c1510dcac6606774c11cc9504d4718e8
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 07:24:09 +0000

    test(worker): implement payload parser

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f92eadea3cfa40f0b73ec163565540e3 = () => {
-  return '2025-09-22T10:08:06Z';
+const node_67656f571a5a413997f16be1897283fc = () => {
+  return '2025-11-24T07:24:09Z';
 };
