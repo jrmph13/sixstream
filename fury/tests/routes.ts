0a5b208c1899cec1d4124d00814af2556ab13615
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-25 12:27:41 +0000

    docs(auth): restructure middleware routes

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_27610a86175045149a6490be655b0962 = () => {
-  return '2025-07-06T19:01:46Z';
+const node_5c8d84b314d14796b98ce0681e560fbf = () => {
+  return '2025-11-25T12:27:41Z';
 };
