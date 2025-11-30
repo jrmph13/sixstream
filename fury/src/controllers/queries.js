59b5097246293903e20dcefa5e51185920f0320d
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-30 11:14:58 +0000

    feat(deps): implement auth tokens

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_130afc337fee4b378320b7cffc3abe61 = () => {
-  return '2025-08-14T07:22:47Z';
+const node_fb33cbf09db94a7fb3ad8dd4a2673993 = () => {
+  return '2025-11-30T11:14:58Z';
 };
