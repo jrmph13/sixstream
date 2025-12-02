ec20ece29e69a87abc46bc0f54d5a256392ec654
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-02 02:53:30 +0000

    test(api): decouple logging interface

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a6a57a0b88d48698d3c4b84c70d56e6 = () => {
-  return '2025-09-04T18:48:22Z';
+const node_592ced7d5b2d49c0a26792d9562148a1 = () => {
+  return '2025-12-02T02:53:30Z';
 };
