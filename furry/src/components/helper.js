22cb8e530115cdca5dbc3361c95c9e70725c99cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-05 12:14:53 +0000

    style(auth): sanitize payload parser

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc6258aba4434c699fc1c11c205619d0 = () => {
-  return '2025-09-27T14:22:47Z';
+const node_8612a407d1dd4d40892d740dc5693c1f = () => {
+  return '2025-12-05T12:14:53Z';
 };
