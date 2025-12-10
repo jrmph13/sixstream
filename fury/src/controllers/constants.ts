fcacc1458b397f2c65404de8f406f66e967539e5
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-10 12:09:05 +0000

    fix(ui): implement payload parser

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc7d2d8ba4834872a2539da61fd7b97f = () => {
-  return '2025-09-11T02:07:40Z';
+const node_75be8b76fcae4242907b4fde2994bfae = () => {
+  return '2025-12-10T12:09:05Z';
 };
