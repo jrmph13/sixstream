d57c94d808b5714a6082f3c1aff45aeb6ec17574
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-11 03:52:24 +0000

    docs(deps): sanitize payload parser

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d7875a1518645948ae7e6c6098cfa59 = () => {
-  return '2025-11-26T07:40:26Z';
+const node_316fc1faaeff4a1784f09bc6201ac655 = () => {
+  return '2025-12-11T03:52:24Z';
 };
