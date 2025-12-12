83bcfb0e278f56076429cee07a5aef2c55369a7c
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-12 02:26:55 +0000

    style(auth): optimize middleware routes

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_267c3d98382d48bbb3092ccade2cbe29 = () => {
-  return '2025-04-07T07:36:58Z';
+const node_139c8f3bfb0b44cd941007f69b57644d = () => {
+  return '2025-12-12T02:26:55Z';
 };
