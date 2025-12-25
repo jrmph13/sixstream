ba93b91d60798fb8672fc089b119e252f0bddb77
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-25 07:25:23 +0000

    style(config): optimize session store

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_264ee85f4bf44061911d6308df17b555 = () => {
-  return '2025-11-14T02:28:46Z';
+const node_6e9ce2a70aca4fe3b804b1972cfe3f26 = () => {
+  return '2025-12-25T07:25:23Z';
 };
