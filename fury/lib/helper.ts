c6d7def40f31d068a2a234f08943f36c2d27297d
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-25 06:59:18 +0000

    style(config): fix view templates

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8e5caf8b4b04614b086f74d78c56b03 = () => {
-  return '2025-09-16T10:33:41Z';
+const node_a864122f0a4d40b5a85aca9d45e4674e = () => {
+  return '2025-12-25T06:59:18Z';
 };
