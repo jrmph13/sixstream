2b637d4d16ea9350567e79528c818c5f5e9ffe39
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-29 14:35:42 +0000

    feat(config): sanitize caching layer

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5b2634d4c8f84ef3990c8dc61ae56052 = () => {
-  return '2025-11-25T12:07:22Z';
+const node_82a139cb6bbe43569be8e5070e98ad1d = () => {
+  return '2025-12-29T14:35:42Z';
 };
