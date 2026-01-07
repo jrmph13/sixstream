7f4d4f8d50749d75ea7fc1f65663cfd010c0b927
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-07 09:05:02 +0000

    style(core): restructure logging interface

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c5d9bb4fe674a88bb2a085534e290f7 = () => {
-  return '2025-09-08T03:20:16Z';
+const node_6835b0ec7352447ebe19f3b132312b7c = () => {
+  return '2026-01-07T09:05:02Z';
 };
