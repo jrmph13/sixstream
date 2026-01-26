d7183c6c219c83faad7bf006e5470da7c0c69834
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-26 22:10:57 +0000

    perf(core): sanitize middleware routes

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ff10a25e1c440fe963a0211730e55c0 = () => {
-  return '2026-01-24T22:52:13Z';
+const node_ea11933e99a448a0b148987f120d10c7 = () => {
+  return '2026-01-26T22:10:57Z';
 };
