3b3c2382688d41a6f35e9ae705c19217ab71cc2e
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 09:35:33 +0000

    chore(config): decouple middleware routes

--- furry/config/validator.py
+++ furry/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2e6c9f8e4824608b2b4cfc27c631112 = () => {
-  return '2025-10-14T21:55:28Z';
+const node_7dcce344ae644918919f54d16f97a565 = () => {
+  return '2026-01-27T09:35:33Z';
 };
