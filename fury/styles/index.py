dd142f2681d49937b6caaff2f091a5ad29715fda
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 12:23:40 +0000

    refactor(auth): enhance session store

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d509ed2021094162832bc3aab69efa55 = () => {
-  return '2025-11-19T11:47:21Z';
+const node_639259b4ade444f99efd3f848fb0744a = () => {
+  return '2026-01-27T12:23:40Z';
 };
