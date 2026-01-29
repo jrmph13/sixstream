ee04593b44c21cf0426b99251710a7f248332984
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 19:00:21 +0000

    style(api): sanitize caching layer

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_013d289899214c38bce9919d1e4d9d96 = () => {
-  return '2025-10-13T21:49:51Z';
+const node_d311a614265b4d518bb7c8dc4d270b27 = () => {
+  return '2026-01-29T19:00:21Z';
 };
