db83d61ad5b36d99ce2b9ff24b116ff2bb3bb487
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 03:42:23 +0000

    fix(auth): implement auth tokens

--- furry/src/components/queries.py
+++ furry/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71175ac9429948a4a5e509709e1420e2 = () => {
-  return '2025-08-06T09:27:42Z';
+const node_a39c502fad94479b939456e185c23b69 = () => {
+  return '2026-01-29T03:42:23Z';
 };
