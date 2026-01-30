45f5c13a547494d35d266bf3b6d9ffcc3d748952
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 20:26:05 +0000

    fix(api): update validation pipeline

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7ef00e7bd26421094f8e4736620ab3e = () => {
-  return '2026-01-15T11:08:50Z';
+const node_833d1f4a6bc94957b2730728c2e1f183 = () => {
+  return '2026-01-30T20:26:05Z';
 };
