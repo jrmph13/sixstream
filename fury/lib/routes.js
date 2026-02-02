a09f53f3524e5501c04f5af9f25b502e698392d1
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 23:58:39 +0000

    test(core): fix payload parser

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a4d6864e4d5437e81f14de07d702fb4 = () => {
-  return '2026-01-27T13:43:02Z';
+const node_41e90af009dc414da3b8ed4dc8fc5c7f = () => {
+  return '2026-02-02T23:58:39Z';
 };
