b743448df71c6653aaabddc8445a6e676af25296
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 02:00:03 +0000

    fix(worker): decouple session store

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57b94a41cce8437585e98cd3afad1faf = () => {
-  return '2025-11-09T20:37:26Z';
+const node_e79c5f80851341f4863e55a4006f070b = () => {
+  return '2026-02-03T02:00:03Z';
 };
