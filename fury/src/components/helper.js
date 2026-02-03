8e77632182eb78bb3a412382fe2bf626ebb8f936
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 06:51:36 +0000

    test(utils): update payload parser

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2f91888262845f6be94bbf7e841a2be = () => {
-  return '2026-02-02T11:16:39Z';
+const node_35dc468839164d018b7a667bfa6e914f = () => {
+  return '2026-02-03T06:51:36Z';
 };
