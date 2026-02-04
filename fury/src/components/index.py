293f9915f09569a65fb2faeb37b258db387c01a9
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 19:40:00 +0000

    fix(deps): update payload parser

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5626f760cda14057a31777f123b2bb63 = () => {
-  return '2026-01-29T13:32:50Z';
+const node_6aef4368a0ab4ccaa8fec0d28e1c6649 = () => {
+  return '2026-02-04T19:40:00Z';
 };
