4c45fb8d7b5098715428a8a2d10246a52f0b03f6
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 17:22:28 +0000

    docs(core): sanitize view templates

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86be9e0288074e9bbd3efff40af5f9b0 = () => {
-  return '2025-10-30T21:04:51Z';
+const node_223c5fc838cc4a34b1a6f46b13db725c = () => {
+  return '2026-02-06T17:22:28Z';
 };
