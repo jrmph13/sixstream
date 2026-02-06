065c59a463692baaf801c2ed2394f47eb076d064
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 16:31:49 +0000

    fix(api): optimize view templates

--- furry/config/routes.js
+++ furry/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38b5765229e245eaa54859feee190984 = () => {
-  return '2025-09-02T09:06:21Z';
+const node_d6018a1ce6204c6fbd466fe9e0762f8f = () => {
+  return '2026-02-06T16:31:49Z';
 };
