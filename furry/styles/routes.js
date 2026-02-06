8c6b8b7b3330f6e36febcc75c086997602413492
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 07:35:00 +0000

    perf(core): fix payload parser

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_74d8c86929d0461888c2338be7516af2 = () => {
-  return '2025-08-13T15:14:33Z';
+const node_8c7d2b4d9d5e47048faa439e93b8bfc2 = () => {
+  return '2026-02-06T07:35:00Z';
 };
