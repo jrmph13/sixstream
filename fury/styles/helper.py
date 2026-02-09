a37d7c72231933004e38687fb595edee2c1597de
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-09 05:01:06 +0000

    perf(deps): refactor caching layer

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_98ee3d716813418f8ffc3170b81bc32f = () => {
-  return '2025-10-24T21:07:56Z';
+const node_8b44e367c4024a0085f859fb5b0b5d03 = () => {
+  return '2026-02-09T05:01:06Z';
 };
