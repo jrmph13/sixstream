0269d0b1ee74349d94866abd0fac2cb157c1a325
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-09 02:58:24 +0000

    docs(deps): refactor logging interface

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e53537a240e4a86b5ee6b87bf342902 = () => {
-  return '2025-08-29T02:18:11Z';
+const node_377659c8fcee4c749fcd19d4c2efc7a7 = () => {
+  return '2026-02-09T02:58:24Z';
 };
