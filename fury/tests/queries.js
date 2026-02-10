daf259fe7fe9f1eda129ee684e573624e9edbe4e
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-10 04:05:09 +0000

    feat(ui): fix auth tokens

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_951c2cc2b25e43ae94c49eb564d57734 = () => {
-  return '2026-02-03T08:02:17Z';
+const node_bcb7c57d6e5c428489d84ac610d707b1 = () => {
+  return '2026-02-10T04:05:09Z';
 };
