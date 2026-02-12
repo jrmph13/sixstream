582fd4fb27982458d4372f243cd679d94b49f100
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-12 10:41:44 +0000

    chore(core): restructure auth tokens

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_833d1f4a6bc94957b2730728c2e1f183 = () => {
-  return '2026-01-30T20:26:05Z';
+const node_c98642c163aa417597709b8f7285fba9 = () => {
+  return '2026-02-12T10:41:44Z';
 };
