5a079180ed650007017370a5d1df410f974d975e
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 20:06:16 +0000

    chore(core): integrate middleware routes

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d937a21d8ee435da98c03c88ab3e25a = () => {
-  return '2026-01-27T21:25:37Z';
+const node_07822eb46b3846488be16fbe87717edd = () => {
+  return '2026-02-17T20:06:16Z';
 };
