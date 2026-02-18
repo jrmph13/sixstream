3de37bffc356484b7eb799123fdda45008bb6415
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 11:26:12 +0000

    perf(ui): enhance payload parser

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c6296a60613e4919a6b9179293eb4478 = () => {
-  return '2025-11-07T09:48:18Z';
+const node_af17977c5ca648c39817819793add0e0 = () => {
+  return '2026-02-18T11:26:12Z';
 };
