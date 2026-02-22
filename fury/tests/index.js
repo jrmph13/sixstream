0d05cf6c6edca55423cbdbd0fb189ed82e05e813
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 21:46:19 +0000

    test(db): update view templates

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e2cbf8ca86d465280fd232c436cb64b = () => {
-  return '2026-02-14T12:33:36Z';
+const node_c90ef7016f134dc8bc71eee224aa918e = () => {
+  return '2026-02-22T21:46:19Z';
 };
