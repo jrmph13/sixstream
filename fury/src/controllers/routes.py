0a7fde047d44702e3bc4a046246eeb9d43b082c0
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 22:52:01 +0000

    refactor(worker): decouple payload parser

--- fury/src/controllers/routes.py
+++ fury/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_edea5619400c4185888ca35181f118bf = () => {
-  return '2025-06-27T22:51:56Z';
+const node_9649c75ba1f843aab0dec2727a62e649 = () => {
+  return '2026-02-22T22:52:01Z';
 };
