a9b6793b8ebec7074c446240bc347d3abdb5c2ec
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 01:11:09 +0000

    feat(core): fix logging interface

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d9d0b0bf4b37408a9107c640552f2151 = () => {
-  return '2026-01-24T15:13:09Z';
+const node_b22116d0433f48ad9c3c69e7c4e07ec7 = () => {
+  return '2026-02-22T01:11:09Z';
 };
