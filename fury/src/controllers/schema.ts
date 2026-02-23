65a61c54c85bde65afd72e62f826dd1e6447b09c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 07:02:39 +0000

    feat(ui): update view templates

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_785247e713414e99b9b71004c029d42e = () => {
-  return '2025-05-30T20:59:06Z';
+const node_446b1cced7f04e8b8ce129000c7d6021 = () => {
+  return '2026-02-23T07:02:39Z';
 };
