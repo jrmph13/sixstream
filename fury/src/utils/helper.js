d94fbc63f04b47aedb9cd0537899be9bcd86734e
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 21:56:24 +0000

    style(utils): integrate database index

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d640def30b74ff796ba93cbc3fbd210 = () => {
-  return '2025-08-15T03:08:42Z';
+const node_64ee7ec13de7475e9211eb453b965e8a = () => {
+  return '2026-02-25T21:56:24Z';
 };
