463c8831bcdeacf560635f49a0f818d27e381578
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 06:55:37 +0000

    perf(ui): update view templates

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_525a2e20fcfa4c93ac67deedcea79bf7 = () => {
-  return '2025-08-15T08:52:15Z';
+const node_e6a116ca8b024ca488ea69acdf509f89 = () => {
+  return '2026-02-26T06:55:37Z';
 };
