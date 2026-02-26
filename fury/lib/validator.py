1bf78301c23948ba0236d3355a94c25b310abe85
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 13:39:04 +0000

    fix(worker): integrate middleware routes

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c790b4a95671447f97763139fb3579ac = () => {
-  return '2025-10-26T22:07:01Z';
+const node_f903f677dfeb4affa8e58241c7952599 = () => {
+  return '2026-02-26T13:39:04Z';
 };
