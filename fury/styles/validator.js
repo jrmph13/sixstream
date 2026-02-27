a87cd77a9c1c4b91ab79856659de2115a4fa4878
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 05:45:26 +0000

    style(core): sanitize payload parser

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dde215ac22b34708be1163e947bc820b = () => {
-  return '2025-08-12T21:07:42Z';
+const node_22f8bca0d91c41e78e1754e51b4880bd = () => {
+  return '2026-02-27T05:45:26Z';
 };
