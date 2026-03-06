da659c18f2e14887accc2eb28f89fa7621d29314
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 15:12:42 +0000

    fix(auth): fix view templates

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7731dee42e6c46c88059c13f0071aa4d = () => {
-  return '2026-02-12T13:03:11Z';
+const node_0c56b94d88d84037abbe8b4038077605 = () => {
+  return '2026-03-06T15:12:42Z';
 };
