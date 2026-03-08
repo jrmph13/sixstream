99fb6ee334cbc40b270ace1826431097938851e7
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 22:39:37 +0000

    style(ui): refactor middleware routes

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30ea1a68c7454620b64bffcf5201951e = () => {
-  return '2026-01-24T02:56:55Z';
+const node_1502cc8bde7442eca0615a4b520932ac = () => {
+  return '2026-03-08T22:39:37Z';
 };
