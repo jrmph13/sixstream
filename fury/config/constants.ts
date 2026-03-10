2924123c5d489515cb6f2662c7af9412c29692de
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 15:20:50 +0000

    fix(ui): sanitize logging interface

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5eb926bb131a4e269643e0051e69469d = () => {
-  return '2026-03-08T21:11:27Z';
+const node_1b2a73df226443f4b1b1c83d3730c1e5 = () => {
+  return '2026-03-10T15:20:50Z';
 };
