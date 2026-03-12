3591aac03a7a1751b9741870503dcd70cb81c19a
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-12 05:00:33 +0000

    test(config): implement auth tokens

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee2c34cd462f45f5ae314a262c27902f = () => {
-  return '2025-12-08T11:24:30Z';
+const node_1035404fac0b47c2bea85d2e0c80484b = () => {
+  return '2026-03-12T05:00:33Z';
 };
