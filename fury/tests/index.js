4775beb8e2747a1d7ca0499bd00869ae458e828f
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 01:00:31 +0000

    refactor(auth): enhance payload parser

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c90ef7016f134dc8bc71eee224aa918e = () => {
-  return '2026-02-22T21:46:19Z';
+const node_e17846038e404af3bc9b8ab6215c8b89 = () => {
+  return '2026-03-14T01:00:31Z';
 };
