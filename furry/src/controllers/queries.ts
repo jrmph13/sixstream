aefb14e43d3500ba4d94a78ffdef41472d8870c2
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-15 06:46:55 +0000

    perf(worker): decouple auth tokens

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c56b94d88d84037abbe8b4038077605 = () => {
-  return '2026-03-06T15:12:42Z';
+const node_fd0697a047b04eddb3d47945046cfcf5 = () => {
+  return '2026-03-15T06:46:55Z';
 };
