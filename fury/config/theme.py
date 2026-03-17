2bf9ab26541c3ce26d43eb32ebe22fec480914f8
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 14:33:05 +0000

    fix(auth): enhance payload parser

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96b13f6fbbbc4d898c008b21da2c1e40 = () => {
-  return '2025-08-13T22:11:19Z';
+const node_df77b93352bc47ea8746d8597da49b9a = () => {
+  return '2026-03-17T14:33:05Z';
 };
