45d6efbf67e098b9473230403c77818c18f8f01a
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 21:17:14 +0000

    style(core): refactor middleware routes

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_13d2964b84154e6b8cf5da884271e7ea = () => {
-  return '2026-02-09T13:16:35Z';
+const node_d334e3bc6b58409983ceac18ede23eee = () => {
+  return '2026-03-26T21:17:14Z';
 };
