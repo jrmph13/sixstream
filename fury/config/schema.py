8db673243f60a13d97a544f9f667a86719791180
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-27 18:39:43 +0000

    test(auth): update view templates

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a90c31e8550941e087727eb4d731c290 = () => {
-  return '2026-02-19T09:42:23Z';
+const node_78c6684c48914b0682e6a7c7d7e9ab59 = () => {
+  return '2026-03-27T18:39:43Z';
 };
