b499bd5dc1a3c508f7f4bfaaa4d39d7a97dc6448
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-10 06:42:29 +0000

    refactor(api): decouple payload parser

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf1f88b40fef464a9dab37d69b49fa59 = () => {
-  return '2026-01-29T20:25:17Z';
+const node_b4d91067f41844b4858a38e7260a08db = () => {
+  return '2026-04-10T06:42:29Z';
 };
