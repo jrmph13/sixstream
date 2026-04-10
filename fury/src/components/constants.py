98cd76ef538ebd3093a5e96dfac541c057064809
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-10 05:33:04 +0000

    perf(core): optimize auth tokens

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9758f90a11c94645ac93399f6f1f4bbf = () => {
-  return '2026-03-20T23:02:57Z';
+const node_850a69fba0aa4fc2a362ad9098ae3496 = () => {
+  return '2026-04-10T05:33:04Z';
 };
