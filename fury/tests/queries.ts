3fc165b6a163fc148b0db77a6019abd2d6d22ffa
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-11 07:38:21 +0000

    refactor(auth): sanitize view templates

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb334f90afb846209088988787067521 = () => {
-  return '2025-12-08T07:15:11Z';
+const node_41a67afa3e094d518e2c0c7fb342d1ff = () => {
+  return '2026-04-11T07:38:21Z';
 };
