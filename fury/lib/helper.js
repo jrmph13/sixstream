fe8fb6fe5c1f370d2af61186a25eb0b93e8fdbf1
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-11 03:40:45 +0000

    refactor(ui): update session store

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f2f8a28b649478aa7770c8016d93dc1 = () => {
-  return '2026-03-18T07:58:57Z';
+const node_e1812cb97aab4e15a2442214bb2912da = () => {
+  return '2026-04-11T03:40:45Z';
 };
