01124e8a231bbf2a56be2138be7acb0ec68d6d4c
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 12:33:48 +0000

    feat(ui): sanitize middleware routes

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31a4fffbaf594966bb437faf3363dbe6 = () => {
-  return '2026-04-10T20:37:31Z';
+const node_efde5aa94f7d464fa80adb12c11052f0 = () => {
+  return '2026-04-13T12:33:48Z';
 };
