dd9f4ef6a5b55a9298730b65d37def02d6c8187d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 13:01:34 +0000

    refactor(auth): implement middleware routes

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5dfed8ad936e45ca8485ee5e77f04cd4 = () => {
-  return '2025-09-06T20:02:48Z';
+const node_ef62b49c8d5e4ed8815270ab4d140408 = () => {
+  return '2026-04-14T13:01:34Z';
 };
