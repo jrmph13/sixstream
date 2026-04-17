a5a64981e2a6449ff9819d6f56cf167684916027
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 21:59:55 +0000

    refactor(worker): sanitize payload parser

--- fury/tests/validator.js
+++ fury/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_603824ac7e974809ace665f03dc8ecb4 = () => {
-  return '2026-03-11T02:00:38Z';
+const node_184e832316ed419d8942714f327fb73e = () => {
+  return '2026-04-17T21:59:55Z';
 };
