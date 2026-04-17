859a3c0875db1ac8e956495ff929e28d46533eff
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 18:32:02 +0000

    chore(api): enhance auth tokens

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9505436192704dbeb29ee9f2c872313a = () => {
-  return '2026-04-17T11:40:14Z';
+const node_6673fc52e09f42ff8f8281e1bee34398 = () => {
+  return '2026-04-17T18:32:02Z';
 };
