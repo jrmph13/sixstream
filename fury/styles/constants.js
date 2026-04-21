3da23bb1acc2de2207217d81e98376521a0fb6de
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 20:39:37 +0000

    refactor(worker): fix payload parser

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6a116ca8b024ca488ea69acdf509f89 = () => {
-  return '2026-02-26T06:55:37Z';
+const node_e7beced616bc419c831e5d206d22b247 = () => {
+  return '2026-04-21T20:39:37Z';
 };
