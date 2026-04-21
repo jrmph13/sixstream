3d3a15ef26f8d77a2049910e9dc59313a4bd179a
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 08:33:28 +0000

    chore(auth): sanitize database index

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a05f050e46b044c1b3107d08a014b8ed = () => {
-  return '2026-03-15T06:47:53Z';
+const node_2d4173921b1c47d1934eff1706f352a2 = () => {
+  return '2026-04-21T08:33:28Z';
 };
