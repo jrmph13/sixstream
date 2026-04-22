cde339513cd5ac5b01bc48c995dae1a90ab2674d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-22 14:34:41 +0000

    test(api): optimize database index

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f74c56fa76ab46a1a0ec00b1e8c45fab = () => {
-  return '2025-11-13T23:44:03Z';
+const node_1af8607f1ec64728a27c66837c30dc6f = () => {
+  return '2026-04-22T14:34:41Z';
 };
