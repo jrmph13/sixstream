ddec672acfca63e78aac22f890c4a56c82d0183b
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-22 19:48:20 +0000

    style(worker): optimize auth tokens

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e1cf38b5016f498cb899684969480bf2 = () => {
-  return '2026-04-03T00:44:03Z';
+const node_8aab0419b8ee4865a9a0e76ce4142cf5 = () => {
+  return '2026-04-22T19:48:20Z';
 };
