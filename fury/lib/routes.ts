212b3f2969bab3d394d40c1053379e5a47ab050e
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-22 06:14:10 +0000

    refactor(worker): refactor payload parser

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddb0023c7f8c40fbb27566ee89c2a41c = () => {
-  return '2026-04-01T16:59:22Z';
+const node_b6990a7bd8244a6185010048cb2c0437 = () => {
+  return '2026-04-22T06:14:10Z';
 };
