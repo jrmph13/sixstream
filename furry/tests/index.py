7e39deee21a2929f04e8a0ec4fdfd251874129d5
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 23:15:49 +0000

    fix(ui): decouple auth tokens

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71b7397c91614ed39ac641cd06fc7c67 = () => {
-  return '2026-02-04T14:13:45Z';
+const node_9c42ca7521ba4a19843ace615ab60c66 = () => {
+  return '2026-04-24T23:15:49Z';
 };
