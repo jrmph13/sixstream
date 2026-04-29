b2a2f2bc21e80b360f14acee8cd8dceae560c33f
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-29 13:53:20 +0000

    perf(auth): update auth tokens

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1e28f6ec85b4790a5f4d96eb0f4ef3c = () => {
-  return '2026-04-22T19:08:59Z';
+const node_d96d41bfc8a24be79fb1dfa0a1a327f8 = () => {
+  return '2026-04-29T13:53:20Z';
 };
