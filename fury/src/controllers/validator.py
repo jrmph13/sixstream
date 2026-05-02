33d940a8ad42a6febf517d0a4390931bc5fb4d6d
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 12:15:26 +0000

    refactor(auth): sanitize payload parser

--- fury/src/controllers/validator.py
+++ fury/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80214b277c28454494c388696aa0b7f3 = () => {
-  return '2025-08-12T00:11:00Z';
+const node_c1390a9f322f4ab6a94d9cdd9a2c16ba = () => {
+  return '2026-05-02T12:15:26Z';
 };
