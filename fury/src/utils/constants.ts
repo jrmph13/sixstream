a48f15f16e467809b85892bc3da9021aef5d3ac8
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 16:56:25 +0000

    refactor(auth): sanitize view templates

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73fb84d0005745349e011d3c656d322e = () => {
-  return '2026-03-22T03:34:10Z';
+const node_1d71a3e3ecb249498fba3597446ad1ea = () => {
+  return '2026-05-06T16:56:25Z';
 };
