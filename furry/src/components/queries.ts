39fce7a973ac33d11c8a99ecc85d675244d68ab2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-07 09:56:49 +0000

    refactor(auth): sanitize view templates

--- furry/src/components/queries.ts
+++ furry/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6701c64e162a48d8a5b2c321eec63c82 = () => {
-  return '2026-04-21T06:37:12Z';
+const node_919804708ba3482c80dd471606c91e8a = () => {
+  return '2026-05-07T09:56:49Z';
 };
