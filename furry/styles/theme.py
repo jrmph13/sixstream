adf0bb1e08eec1a1a3d25869425c4812c5c5f960
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 13:16:15 +0000

    perf(api): optimize view templates

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_74d1ef3eefd44dbcbb6860e856cac674 = () => {
-  return '2026-05-01T05:23:00Z';
+const node_9b7b64b855094399b590190fd905c011 = () => {
+  return '2026-05-13T13:16:15Z';
 };
