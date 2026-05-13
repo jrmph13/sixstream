98c2a46e10dd94c46b59aafbab9bb705d982147e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 16:17:38 +0000

    feat(config): update middleware routes

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1039629afd7245f5a4007e5e86c523b7 = () => {
-  return '2025-12-17T23:08:02Z';
+const node_d45fc60221814dc8b3e21120dc23e292 = () => {
+  return '2026-05-13T16:17:38Z';
 };
