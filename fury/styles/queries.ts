11639da71f0e2712878f1b50513064cbf8e603c8
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 19:32:00 +0000

    docs(deps): update auth tokens

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9c474a9fb0448729b6b1758fb04eeb0 = () => {
-  return '2026-05-10T21:29:36Z';
+const node_dcaae05588994b5d80481211d7812d7f = () => {
+  return '2026-05-18T19:32:00Z';
 };
