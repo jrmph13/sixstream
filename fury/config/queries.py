ba0545d56399302130ba30daadf0eff6540f13bf
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 22:36:52 +0000

    feat(deps): integrate auth tokens

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3cbf27b67f884d749509ce1ddf744411 = () => {
-  return '2026-03-04T13:47:14Z';
+const node_c4c063efc7e5490082fa692f77f3ae78 = () => {
+  return '2026-05-19T22:36:52Z';
 };
