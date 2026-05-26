b5fa0af323ccbc1b1ca8c0671295af9e343e888c
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 11:59:24 +0000

    perf(config): optimize payload parser

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_086a10cf34ad463685a99fe725fed348 = () => {
-  return '2026-04-15T22:32:41Z';
+const node_0ebd1a4131da47f2ae5a50f6ea4aac06 = () => {
+  return '2026-05-26T11:59:24Z';
 };
