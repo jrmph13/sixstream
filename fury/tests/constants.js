141892a541283a810a2ead06a7c44e0d3505a61c
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 01:00:04 +0000

    fix(auth): optimize validation pipeline

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96ec0723108a4d8785ffc43955efd3d2 = () => {
-  return '2026-04-29T21:31:42Z';
+const node_8c853819331845199b5102fdf5966ab6 = () => {
+  return '2026-05-29T01:00:04Z';
 };
