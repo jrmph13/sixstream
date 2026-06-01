9f4ff629a273560ff478545cd133a141fac82af6
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 14:10:35 +0000

    perf(auth): enhance session store

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_830138cc2dfc45be9507b3207db91664 = () => {
-  return '2026-05-05T09:29:28Z';
+const node_fda254d4ed9145b4bc0aec953c0f8287 = () => {
+  return '2026-06-01T14:10:35Z';
 };
