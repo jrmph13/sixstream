f1873cf1b8cfd183afc46d259cc4ea25e58d52a7
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 16:14:28 +0000

    style(api): refactor caching layer

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed684e37022849529ca5f6918c2d78d0 = () => {
-  return '2026-05-26T01:38:52Z';
+const node_39beb7dfb75043e1928202e722c257d8 = () => {
+  return '2026-06-02T16:14:28Z';
 };
