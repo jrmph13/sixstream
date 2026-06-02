0298aee51942e4400741c550dbf95260d158b63a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 21:39:33 +0000

    style(utils): update view templates

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c8c95b419a7b4c4e99aa38619f1a475b = () => {
-  return '2026-06-01T05:49:58Z';
+const node_2cf203fdeb5844f98cbf14fd7136f58a = () => {
+  return '2026-06-02T21:39:33Z';
 };
