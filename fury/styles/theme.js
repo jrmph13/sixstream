c351479d0ec9ec60c4ee37a9b7101f1c152a911f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 19:41:50 +0000

    docs(api): sanitize middleware routes

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39beb7dfb75043e1928202e722c257d8 = () => {
-  return '2026-06-02T16:14:28Z';
+const node_2fd1add055154a8fabf97f790c2b81f8 = () => {
+  return '2026-06-03T19:41:50Z';
 };
