11a690bf88c35de83f8e9c03bf393c35daffbbb7
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 00:55:46 +0000

    refactor(auth): restructure caching layer

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f070c9ce09b4388ba610af1bde0198e = () => {
-  return '2026-04-17T20:05:14Z';
+const node_ee7fa6fad7dc47d085d82337cb135dfa = () => {
+  return '2026-06-04T00:55:46Z';
 };
