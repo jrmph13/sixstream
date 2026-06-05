173b43b7be9847a00d3bda58a5bb45bfde4d656a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 17:19:19 +0000

    chore(auth): update caching layer

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31ae35ba729343a8abf62963117fe40f = () => {
-  return '2026-04-24T04:27:59Z';
+const node_2a8f9ccc38c840799ca08accb93b25c9 = () => {
+  return '2026-06-05T17:19:19Z';
 };
