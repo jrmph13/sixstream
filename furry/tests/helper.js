8afa73c93429fa06976a6af8082b9b87a8808a65
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 03:05:38 +0000

    perf(auth): refactor auth tokens

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3797a9f3421a4a2c812f1afe934755c8 = () => {
-  return '2026-05-06T11:28:24Z';
+const node_eba5ed70da074deba92e2d5f1e212fcd = () => {
+  return '2026-06-06T03:05:38Z';
 };
