abd7040f2266cbd5337f300af42b7f4e37e9acf6
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-09 23:16:31 +0000

    refactor(auth): integrate payload parser

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50d9f31af5aa461f9fe143874be3049f = () => {
-  return '2026-06-05T11:10:08Z';
+const node_ef0e4d2de5d742229beb568917d8c74b = () => {
+  return '2026-06-09T23:16:31Z';
 };
