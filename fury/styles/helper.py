42c0e30bb1a65871a650f1463ff622d887906361
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-09 20:48:42 +0000

    style(db): sanitize middleware routes

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b44e367c4024a0085f859fb5b0b5d03 = () => {
-  return '2026-02-09T05:01:06Z';
+const node_8cdc8ff0f8bd4c2ab03d2ebc4daf1fa7 = () => {
+  return '2026-06-09T20:48:42Z';
 };
