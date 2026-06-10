1d4c84477345b8ff808f45120a6620b057238374
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 18:05:36 +0000

    chore(config): decouple payload parser

--- furry/config/validator.js
+++ furry/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_89458e449cca4edd9de497eb2f7082c0 = () => {
-  return '2026-06-01T17:41:38Z';
+const node_e7c0fb55a5734f35bc16c30f1a479b87 = () => {
+  return '2026-06-10T18:05:36Z';
 };
