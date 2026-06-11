d1f386610388624a0b1e3ca236e2840717582a5f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 09:07:43 +0000

    feat(api): refactor logging interface

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ceb869c27714d10a7a1ae6b1c1a3f70 = () => {
-  return '2026-06-05T13:29:14Z';
+const node_0667ca8e9bd3403090ddb4b5ba1554f1 = () => {
+  return '2026-06-11T09:07:43Z';
 };
