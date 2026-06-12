4bd057640c16dca28b0e40d2d6f2a0042429887d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 02:40:16 +0000

    refactor(api): sanitize payload parser

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e365e77c2684cdb959f72c1c0a28683 = () => {
-  return '2026-06-02T11:22:46Z';
+const node_629dfd568df44769a46b64784297fc6f = () => {
+  return '2026-06-12T02:40:16Z';
 };
