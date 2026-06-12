b33ab051811e31cc77dc44dbed75d56295054837
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 12:54:13 +0000

    fix(db): enhance view templates

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96eedea721f5437fab39950afd5e9fbd = () => {
-  return '2026-06-09T19:24:58Z';
+const node_af4f7ba23f3f4b95ba3715a306b0c95f = () => {
+  return '2026-06-12T12:54:13Z';
 };
