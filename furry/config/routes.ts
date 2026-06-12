fe760f67fe5d2edf556f22afa8ab2ae50b5bd320
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 19:27:26 +0000

    refactor(deps): fix database index

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_371ef39f825848cfaae9694270d44ea4 = () => {
-  return '2026-04-21T14:44:31Z';
+const node_307fcce8fe144481b37f252a3a3605bc = () => {
+  return '2026-06-12T19:27:26Z';
 };
