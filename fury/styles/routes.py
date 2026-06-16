23cdbfb1ea996f79f68d97376ed8caa89be6fd87
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 20:19:42 +0000

    fix(utils): fix view templates

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_334635b0de724210b62ed19293a57c3b = () => {
-  return '2026-01-27T09:08:22Z';
+const node_a93ce6299c7b459881cc45665fecfebd = () => {
+  return '2026-06-16T20:19:42Z';
 };
