46bba00187a0493df36e7dcd66d8f45a97fccf3d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-17 22:48:23 +0000

    feat(core): optimize payload parser

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d5a4a23e5e5c4069b7c0b1e29b17ac5e = () => {
-  return '2026-05-15T06:12:18Z';
+const node_f1fb41e077694094a0b275529b7c7c35 = () => {
+  return '2026-06-17T22:48:23Z';
 };
