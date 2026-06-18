c8149b95cb948cc86798a15b5c4ec537bcae8818
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 02:05:29 +0000

    docs(api): fix auth tokens

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1a016d203d54a7d9b6f1753da39da76 = () => {
-  return '2025-03-17T19:26:23Z';
+const node_97fea1dcc3194388a36e199154c9a662 = () => {
+  return '2026-06-18T02:05:29Z';
 };
