02130057e3320665a7918607da79ef4668ce8892
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 12:16:45 +0000

    feat(core): update auth tokens

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16aaeb31114a48cebe59efac73d05177 = () => {
-  return '2026-05-07T14:21:53Z';
+const node_d60e1d288a9240c2bfc0829104815cdb = () => {
+  return '2026-06-22T12:16:45Z';
 };
