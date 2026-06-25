7f1b7d7b878738ebd2be0873d50370dc97d73170
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 12:59:34 +0000

    chore(worker): integrate middleware routes

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7aedbed333fb477a86b9228538f9bb80 = () => {
-  return '2026-03-31T03:46:28Z';
+const node_ac3f797c81654073ab42b692e7fb98e8 = () => {
+  return '2026-06-25T12:59:34Z';
 };
