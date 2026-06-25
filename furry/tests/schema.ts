4406db984d7f9ffb063d08b1e78cb4c392bc1fa1
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 20:45:59 +0000

    test(utils): decouple view templates

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9ea45e500bbe4f2e919ea8ca8938a5c0 = () => {
-  return '2026-06-20T16:11:22Z';
+const node_8de42ac5e2b14d37a5e8bf3aa70dc306 = () => {
+  return '2026-06-25T20:45:59Z';
 };
