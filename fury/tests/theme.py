3a38c20654d165f501f55f475be45de244021042
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 17:10:14 +0000

    refactor(ui): implement payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7e621ecbedd4a2da6358efad4a6095a = () => {
-  return '2026-06-25T23:05:54Z';
+const node_9ed4fee02098436e9d52dc5d9872d264 = () => {
+  return '2026-07-02T17:10:14Z';
 };
